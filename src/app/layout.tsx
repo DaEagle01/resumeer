8101143c8a7e151a4f6870158c8d97be72613ef3
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-03-09 23:21:55 +0600

    installed shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Inter as FontSans } from "next/font/google"
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: {
@@ -12,6 +12,11 @@ export const metadata: Metadata = {
   description: "Transform your professional narrative into a masterpiece with Resumeer. Our intuitive platform guides you through crafting a resume that resonates with recruiters and showcases your unique career journey.",
 };
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -19,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       Hello Resumeer
+      <Button className="bg-gray-300 text-gray-900 border-2 border-slate-100">Click me</Button>
     </div>
   );
 }
